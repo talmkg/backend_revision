889257ed47be59caf0afa52da2ba7bb987f1bdc5
Tim Afanasiev <soupsoupchannel@gmail.com>
2023-01-07 09:31:57 +0100

    post method added

--- src/authors/index.js
+++ src/authors/index.js
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
+import uniqid from "uniqid"
 import fs from "fs";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const authorsFilePath = path.join(__dirname, "authors.json");
+const authorsFilePath = path.join(__dirname, "authors.json"); 
 
 const router = express.Router();
 
@@ -20,6 +21,28 @@ router.get("/", async (req, res, next) => {
 });
 router.post("/", async (req, res, next) => {
   try {
+    const {name, surname, email, dateOfBirth} = req.body;
+
+    const author = {
+      name,
+      surname,
+      email,
+      dateOfBirth,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: uniqid()
+    };
+
+    const fileAsBuffer = fs.readFileSync(authorsFilePath);
+    const fileAsString = fileAsBuffer.toString();
+    const fileAsJSONArray = JSON.parse(fileAsString);
+
+    fileAsJSONArray.push(author);
+
+    fs.writeFileSync(authorsFilePath, JSON.stringify(fileAsJSONArray)); 
+    res.send(author);
+
+    
   } catch (error) {
     res.send(500).send({ message: error.message });
   }
